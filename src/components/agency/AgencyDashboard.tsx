1c741d77e0f4d69f18dba41dd05a5e128278715a
kishan V <sadakr@plateron.com>
2025-03-27 22:29:27 +0530

    feat: added dashboard anims for ag

--- src/components/agency/AgencyDashboard.tsx
+++ src/components/agency/AgencyDashboard.tsx
@@ -1,19 +1,12 @@
-import { motion } from "framer-motion";
-import { VideoAssets } from "../../constants/AssetLocation.ts";
-import { AgencyConstants } from "../../constants/AgencyConstants.ts";
+import {AgencyConstants} from "../../constants/AgencyConstants.ts";
 import Particles from "../../widgets/Particles/Particles.tsx";
 import Orb from "../../widgets/Orb/Orb.tsx";
+import BlurText from "../../widgets/BlurText/BlurText.tsx";
+import SplitText from "../../widgets/SplitText/SplitText.tsx";
 
-const wordVariants = {
-    hidden: { opacity: 0, y: 20 }, // Start from below
-    visible: (i: number) => ({
-        opacity: 1,
-        y: 0,
-        transition: { delay: i * 0.05, duration: 0.5, ease: "linear" },
-    }),
-};
 
 const AgencyDashboard = () => {
+
     return (
         <div className="w-full h-screen flex items-center justify-center text-center relative overflow-hidden">
 
@@ -33,7 +26,7 @@ const AgencyDashboard = () => {
 
             {/* Orb in the center, behind the text */}
             <div className="absolute inset-0 flex items-center justify-center z-10 pb-16">
-                <Orb 
+                <Orb
                     hoverIntensity={0.5}
                     rotateOnHover={true}
                     hue={90}
@@ -41,29 +34,31 @@ const AgencyDashboard = () => {
             </div>
 
             {/* Centered Text (Above Orb) */}
-            <div className="relative z-20 text-white px-4 max-w-4xl">
-                <h2 className="text-3xl md:text-8xl font-alfaSlab leading-tight">
-                    {AgencyConstants.agencySlogan.split("").map((char, index) => (
-                        <motion.span
-                            key={index}
-                            custom={index}
-                            variants={wordVariants}
-                            className="inline-block"
-                        >
-                            {char === " " ? "\u00A0" : char}
-                        </motion.span>
-                    ))}
-                </h2>
+            <div className="relative z-20 text-white px-4 ">
+                <BlurText
+                    text={AgencyConstants.agencySlogan}
+                    delay={150}
+                    animateBy="words"
+                    direction="top"
+                    className="text-3xl md:text-9xl font-alfaSlab leading-tight text-c"
+                />
+                <div className="mt-4">
+                <SplitText
+                    text=" Crafting exceptional digital experiences for brands that matter."
+                    className=" text-lg md:text-xl opacity-90 font-sans"
+                    delay={25}
+                    animationFrom={{opacity: 0, transform: 'translate3d(0,50px,0)'}}
+                    animationTo={{opacity: 1, transform: 'translate3d(0,0,0)'}}
+                    threshold={0.5}
+                    rootMargin="-50px"
+                /></div>
 
-                <p className="mt-4 text-lg md:text-xl opacity-90 font-sans">
-                    Crafting exceptional digital experiences for brands that matter.
-                </p>
+
             </div>
 
         </div>
     );
 };
-
 
 
 export default AgencyDashboard;
